b977b07b575c5b9c18c4ed4942006f5681f730ac
R. Miles McCain <development-machine@sendmiles.email>
2021-01-14 12:31:54 -0500

    Add output handler

---
+++ src/output.py
@@ -0,0 +1,18 @@
+import json
+import unicodecsv as csv
+from datetime import datetime
+
+global _format
+
+def set_format(format):
+    global _format
+
+    _format = format
+
+def emit(obj: dict):
+    # Serialize keys
+    for key in obj.keys():
+        if type(obj[key]) == datetime:
+            obj[key] = datetime.isoformat(obj[key])
+    
+    print(json.dumps(obj))
