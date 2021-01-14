552665724f8c8a9e13ae8ad40c9de071bc5517b0
R. Miles McCain <development-machine@sendmiles.email>
2021-01-14 13:56:29 -0500

    Add trending command

---
+++ src/commands/trending.py
@@ -0,0 +1,13 @@
+import click
+import requests
+from ..output import emit
+
+
+@click.command("trending")
+def command():
+    data = requests.get("https://dissenter.com/url").json()
+
+    # We currently ignore trending comments.
+    # The pagination here is opaque/doesn't seem to be meaningful, so we only pull once
+    for url in data["trendingUrls"]:
+        emit(url)
