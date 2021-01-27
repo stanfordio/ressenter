6854e997aa992ae299ad28be003953e137dc40d6
R. Miles McCain <development-machine@sendmiles.email>
2021-01-27 11:56:42 -0500

    Add listener-based output system

---
+++ ressenter/__init__.py
@@ -0,0 +1,2 @@
+from .output import set_output, set_format, result_listeners, disable_standard_output
+from .commands import comments, trending, url, user
--- ressenter/output.py
+++ ressenter/output.py
@@ -22,14 +22,11 @@ def set_output(output):
     _output = output
     _csv_writer = None
 
+def disable_standard_output():
+    result_listeners.remove(write_standard_output)
 
-def emit(obj: dict):
+def write_standard_output(obj: dict):
     global _csv_writer
-
-    # Serialize keys
-    for key in obj.keys():
-        if type(obj[key]) == datetime:
-            obj[key] = datetime.isoformat(obj[key])
 
     if _format == "jsonl":
         _output.write((json.dumps(obj) + "\n").encode("utf-8"))
@@ -39,6 +36,21 @@ def emit(obj: dict):
             _csv_writer = csv.DictWriter(_output, obj.keys())
             _csv_writer.writeheader()
         _csv_writer.writerow(obj)
+
+
+result_listeners = [write_standard_output]  # Functions to call on each result
+
+
+def emit(obj: dict):
+    global result_listeners
+
+    # Serialize keys
+    for key in obj.keys():
+        if type(obj[key]) == datetime:
+            obj[key] = datetime.isoformat(obj[key])
+
+    for listener in result_listeners:
+        listener(obj)
 
 
 def close():
