6603594026fb1ee234b3f10ad8da210fb1b16cb6
R. Miles McCain <development-machine@sendmiles.email>
2021-01-28 10:22:07 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="ressenter",
-    version="0.0.5",
+    version="0.0.6",
     description="Toolkit for Dissenter",
     long_description=README,
     long_description_content_type="text/markdown",
