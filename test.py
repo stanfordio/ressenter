6d34b42cc00179afbc81aa0bc3e113b01cb9978f
R. Miles McCain <development-machine@sendmiles.email>
2021-01-28 12:13:54 -0500

    Remove unnecessary test file

--- test.py
+++
@@ -1,9 +0,0 @@
-import ressenter
-import argparse
-import sys
-
-parser = argparse.ArgumentParser(
-    description="Process command line options in conflicting_module.py."
-)
-parser.add_argument("--conflicting", "-c")
-parser.parse_args(sys.argv)
